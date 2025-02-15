1d04bdbda7c5b2e2765cbabc14a88719a5d0f887
SamarthBidaye <bidayesamedu@gmail.com>
2025-02-15 14:10:09 +0530

    Mongoose Key Upload

--- mongoose/mongo.js
+++ mongoose/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connect = mongoose.connect('mongodb+srv://Samarth:bidaye@cluster0.bag0e.mongodb.net/User-Data?retryWrites=true&w=majority&appName=Cluster0');
+const connect = mongoose.connect('mongodb+srv://Samarth:password@cluster0.bag0e.mongodb.net/User-Data?retryWrites=true&w=majority&appName=Cluster0');
 
 const userdata = mongoose.Schema({
     name: String,
